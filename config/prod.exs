b1e30d46e416764a94cea7e97eab4f315dbf30ed
Eddie <emcmanus@users.noreply.github.com>
2023-11-07 06:50:55 -0800

    Debug logging

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Robocoder.Finch
 config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, level: :info
+#config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
